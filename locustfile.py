4b5a3243a72689d026466a2f57e60c4a82106b18
imabgd <imabgd@gmail.com>
2020-12-22 02:11:39 +0000

    locustfile code update

--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,11 @@
-from locust import HttpUser, between, task
+from locust import HttpLocust, TaskSet, between
 
+def index(l):
+    l.client.get("/")
 
-class WebsiteUser(HttpUser):
-    wait_time = between(5, 15)
-    
-    def on_start(self):
-        self.client.post("/login", {
-            "username": "test_user",
-            "password": ""
-        })
-    
-    @task
-    def index(self):
-        self.client.get("/")
-        self.client.get("/static/assets.js")
-        
-    @task
-    def about(self):
-        self.client.get("/about/")
+class UserBehavior(TaskSet):
+    tasks = {index: 1}
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    wait_time = between(5.0, 9.0)
